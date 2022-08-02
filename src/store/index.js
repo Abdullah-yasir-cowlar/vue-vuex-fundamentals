c0a311c77e3d1550d6bea6c057ddda746e8fa46d
abdullah-yasir-cowlar <abdullah.yasir.cowlar@gmail.com>
2022-08-02 12:17:16 +0500

    starter code

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,10 @@
 import { createStore } from 'vuex'
 
 export default createStore({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {}
+    state: {
+        user: 'John Doe'
+    },
+    mutations: {},
+    actions: {},
+    modules: {}
 })
